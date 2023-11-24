c5b3588a6c780d56996f3803dd7e66331f338fac
nbonnec <xxx@gmail.com>
2023-11-24 12:06:57 +0100

    It works.

--- tp10/celery_task.py
+++ tp10/celery_task.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('hello', broker='pyamqp://guest@localhost//')
+app = Celery('hello', broker='pyamqp://guest@localhost//', backend='redis://localhost:6379/0')
 
 
 @app.task
