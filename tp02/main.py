215376831322364e49a13f2d4419d94b84a4189d
nbonnec <xxx@gmail.com>
2023-11-24 12:06:55 +0100

    End of exceptions TP.

--- tp02/main.py
+++ tp02/main.py
@@ -2,23 +2,52 @@
 
 import os
 import sys
+import traceback
 from pprint import pprint
 from utils import new_paragraph
 
 
-def main():
-    new_paragraph('try, except, else, finally')
+class DivisionBy12Error(Exception):
+    def __init__(self):
+        super().__init__('Division by 12!')
+
+
+def call_div(a: int, b: int):
+    print('Open log')
     try:
-        a = int(input('Value of a= '))
-        b = 2
-        c = b / a
+        c = div12(a, b)
+    finally:
+        print('Close log')
+    return c
+
+
+def div(a: int, b: int):
+    return a / b
+
+
+def div12(a: int, b: int):
+    if b == 12:
+        raise DivisionBy12Error()
+    return a / b
+
+
+def main():
+    try:
+        a = 2
+        b = 0
+        c = call_div(a, b)
         print(c)
+    except DivisionBy12Error as e:
+        print('My custom exception')
+        print(e, type(e))
     except Exception as e:
+        traceback.print_exc()
         print(e, type(e))
     else:
         print('No error')
     finally:  # it is better in libraries
         print('finally')
+    new_paragraph('exceptions')
     print('After errors')
 
 
---
+++ tp02/main_3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from pprint import pprint
+from utils import new_paragraph
+
+
+def main():
+    new_paragraph('try, except, else, finally')
+    try:
+        a = int(input('Value of a= '))
+        b = 2
+        c = b / a
+        print(c)
+    except Exception as e:
+        print(e, type(e))
+    else:
+        print('No error')
+    finally:  # it is better in libraries
+        print('finally')
+    print('After errors')
+
+
+if __name__ == '__main__':
+    main()
